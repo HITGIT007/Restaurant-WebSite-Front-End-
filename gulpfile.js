b805c87886aafe9137fbeaf5dcc80d67eb7aaf07
AlphaHIT <prateekpriyadarshi1328@gmail.com>
2021-05-14 16:47:56 +0530

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
 
 gulp.task('sass', function() {
@@ -10,7 +20,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('sass:watch', function() {
-    gulp.watch('./css/*.scss', ['sass']);
+    gulp.watch('./css/*.scss', gulp.series('sass'));
 });
 
 gulp.task('browser-sync', function() {
@@ -30,6 +40,45 @@ gulp.task('browser-sync', function() {
 });
 
 // Default task
-gulp.task('default', ['browser-sync'], function() {
-    gulp.start('sass:watch');
+gulp.task('default', gulp.parallel('browser-sync', 'sass:watch'));
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
 });
+
+gulp.task('copyfonts', function() {
+
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+
+    .pipe(gulp.dest('./dist/fonts'));
+
+});
+// Images
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/img'));
+});
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function(stream, file) {
+            return stream
+                .pipe(usemin({
+                    css: [rev()],
+                    html: [function() { return htmlmin({ collapseWhitespace: true }) }],
+                    js: [uglify(), rev()],
+                    inlinejs: [uglify()],
+                    inlinecss: [cleanCss(), 'concat']
+                }))
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['clean'], function() {
+    gulp.start('copyfonts', 'imagemin', 'usemin');
+});
+
+gulp.task('build', gulp.series('clean', 'copyfonts', 'imagemin', 'usemin', function(done) { done(); }
+
+));
